0b4d05ed7cba0db0993beb499be6b67a84bf8c2b
SebastianOrtiz2194 <sebastian_2194@hotmail.com>
2025-09-09 17:29:19 +0900

    User model creation

---
+++ src/main/java/com/example/taskmanager/model/User.java
@@ -0,0 +1,55 @@
+package com.example.taskmanager.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Document(collection = "users")
+@Data
+@NoArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList(); // For simplicity, we are not using roles
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
