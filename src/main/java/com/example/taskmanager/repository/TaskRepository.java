684ea6d6c6fa5b983ff97c945c6c576239a144ed
SebastianOrtiz2194 <sebastian_2194@hotmail.com>
2025-09-18 16:10:38 +0900

    pom update

--- src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -12,6 +12,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface TaskRepository extends MongoRepository<Task, String> {
-    // You can define custom query methods here if needed.
-    // For example: findByCompleted(boolean completed);
+
 }
